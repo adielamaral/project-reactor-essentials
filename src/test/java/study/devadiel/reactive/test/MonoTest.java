52315c88294aed2e415d94fcaa7ccc59328c7bcf
Adiel Amaral <adielnfs@gmail.com>
2023-06-15 21:00:37 -0300

    As dependências necessárias foram adicionadas e uma classe de teste foi criada

---
+++ src/test/java/study/devadiel/reactive/test/MonoTest.java
@@ -0,0 +1,13 @@
+package study.devadiel.reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class MonoTest {
+
+    @Test
+    public void test() {
+        log.info("Tudo funcionando como planejado");
+    }
+}
